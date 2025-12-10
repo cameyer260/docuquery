c1a0e35961042dd99995424de603ce36050498eb
Christopher Meyer <cameyer06@gmail.com>
2025-12-10 09:11:40 -0600

    force home page and signin page to be dynamic, seperate ui for each into their own components for SEO. had to force dynamic because of the redirect, it was causing errors because nextjs tried to make the pages static, which the nextjs redirect server function cannot be used in a static page. also updated the eslint config to stop using flatcompact and just use the offical nextjs flat config instead which was causing errors in my editor.

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
-import GetStarted from "@/components/landing-page/get-started";
-import GetStartedButton from "@/components/landing-page/get-started-button";
+export const dynamic = "force-dynamic";
+
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import LandingContent from "@/components/landing-page/landing-content";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (session) redirect("/main/upload");
 
-  return (
-    <div className="flex flex-col p-12 gap-12">
-      <div className="flex flex-col text-center gap-4 text-5xl font-bold">
-        <h1>Ask GPT Questions About Your PDFs</h1>
-        <h1>Get Instant, Accurate Answers</h1>
-      </div>
-      <div className="flex flex-col text-center gap-4 text-2xl">
-        <div className="flex flex-col gap-2">
-          <h1>Hate reading your textbook? Let AI do it for you.</h1>
-          <h1 className="font-bold text-3xl">How It Works</h1>
-        </div>
-        <div className="flex flex-col items-center gap-4">
-          {/** Light blue color used for icons and buttons here on the landing page -> #00A1FF */}
-          <GetStarted />
-          <GetStartedButton />
-        </div>
-      </div>
-    </div>
-  );
+  return <LandingContent />;
 }
